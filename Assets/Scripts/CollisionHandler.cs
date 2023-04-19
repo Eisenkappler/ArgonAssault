9a171a1074a99a0542c03c2d6563843a7ba36bdd
Nikolai Pronegg <nikolai.pronegg@student.tugraz.at>
2023-04-19 21:50:30 +0200

    explosion added SelfDestruction script

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
   
+    [SerializeField] ParticleSystem crashVFX;
+    
      private void OnCollisionEnter(Collision other) {
         
         Debug.Log(this.gameObject.name + "collided with " + other.gameObject.name);
@@ -13,8 +16,34 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) {
         Debug.Log(this.gameObject.name + "triggered " + other.gameObject.name);
+        
+       StartCrashSequence();
+
+    }
+    private void StartCrashSequence()
+    {
+        crashVFX.Play();
+        GetComponent<PlayerControls>().enabled = false;
+        foreach (Transform child in transform)
+        {
+         MeshRenderer renderer = child.GetComponent<MeshRenderer>();
+
+         if (renderer != null)
+         {
+            renderer.enabled = false;
+        }
+        }
+        GetComponent<BoxCollider>().enabled = false;
+        Invoke("ReloadLevel",1f);
     }
 
+        void ReloadLevel()
+    {
+       
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        Debug.Log("RELOAD");
+    }
 
     
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
  
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    
     private void OnParticleCollision(GameObject other) {
         Debug.Log($"I'm hit by {other.gameObject.name }");
+        GameObject vfx =  Instantiate(deathVFX,transform.position,Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(this.gameObject);
     }
 
