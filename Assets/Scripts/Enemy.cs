73cd3cfa47a044ab76625535264fb26d3bd0a80e
Nikolai Pronegg <nikolai.pronegg@student.tugraz.at>
2023-04-25 22:11:19 +0200

    enemy prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,22 +8,32 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scoreincrease = 1;
+    [SerializeField] int healthPoints = 3;
 
     ScoreBoard scoreBoard;
+    Rigidbody rigidBody;
 
     private void Start() {
         scoreBoard = FindObjectOfType<ScoreBoard>();   
+        rigidBody  = gameObject.AddComponent<Rigidbody>();
+        rigidBody.useGravity =false;
+        rigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
     
     private void OnParticleCollision(GameObject other) {
         ProcessHit();
-        KillEnemy();
        
     }
 
 
     private void ProcessHit()
     {
+        healthPoints--;
+        if (healthPoints < 0)
+        {
+            KillEnemy();
+            return;
+        }
         scoreBoard.IncreaseScore(scoreincrease);
     }
 
